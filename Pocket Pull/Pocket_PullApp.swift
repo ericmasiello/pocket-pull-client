61cbd0bb6d87a52b52d5c98d00f023915aff2625
Eric Masiello <emasiello@vistaprint.com>
2026-05-16 13:54:47 -0400

    first commit

---
+++ Pocket Pull/Pocket_PullApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_PullApp.swift
+//  Pocket Pull
+//
+//  Created by Eric Masiello on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_PullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
